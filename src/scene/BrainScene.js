59827d17f5d31d0ff6df2babf50f3814f0f11fbb
Fedor Stomakhin <fedor.stomakhin@gmail.com>
2021-05-03 01:00:53 +0300

    add freesurfer-style 3 extra camera views

--- src/scene/BrainScene.js
+++ src/scene/BrainScene.js
@@ -2,20 +2,41 @@ import React, {Component, createRef} from "react";
 import * as THREE from "three";
 import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
 import {GLTFLoader} from "three/examples/jsm/loaders/GLTFLoader";
-import {Grid, GridColumn, Sticky, Ref} from "semantic-ui-react";
+import {Grid, GridColumn, Sticky, Ref, Segment, Header} from "semantic-ui-react";
 import {hiddenIndexes} from "../helpers/Utility";
 import {PageSidebar} from "./PageSidebar";
 import {PageHeader} from "./PageHeader";
 import {Group, Mesh} from "three";
 import UploadBox from "./UploadBox";
+import {Slider} from "react-semantic-ui-range";
 
 const clamp = (val, from, to) => Math.min(Math.max(val, from), to);
+
+const orthographicCameraDirection = {
+  Y: "y",
+  X: "x",
+  Z: "z",
+};
+
+const planePositionByDirection = {
+  [orthographicCameraDirection.X]: (centre, nearOrFar, distance) => {
+    return [centre[0] + (nearOrFar === "near" ? distance : -distance), centre[1], centre[2]];
+  },
+  [orthographicCameraDirection.Y]: (centre, nearOrFar, distance) => {
+    return [centre[0], centre[1] + (nearOrFar === "near" ? distance : -distance), centre[2]];
+  },
+  [orthographicCameraDirection.Z]: (centre, nearOrFar, distance) => {
+    return [centre[0], centre[1], centre[2] + (nearOrFar === "near" ? distance : -distance)];
+  },
+};
+
 
 const sceneStyle = {
   height: 750, // we can control scene size by setting container dimensions
 };
+const initialCameraSpread = 30;
 const totalTime = 10000; // 10s
-
+const planeTimeoutMs = 800; // .8s
 const sprite = new THREE.TextureLoader().load( "sprites/disc.png" );
 const vertexShader = `
 #define PI 3.1415926535
@@ -110,6 +131,16 @@ class BrainScene extends Component {
         blending: THREE.AdditiveBlending,
         sizeAttenuation: true,
       }),
+      topCameraCentre: 0,
+      topCameraSpread: initialCameraSpread,
+      topPlaneTimeout: null,
+      sideCameraCentre: 0,
+      sideCameraSpread: initialCameraSpread,
+      sidePlaneTimeout: null,
+      frontCameraCentre: 0,
+      frontCameraSpread: initialCameraSpread,
+      frontPlaneTimeout: null,
+      orthoCameraDistance: 120,
     });
     this.loadModel = this.loadModel.bind(this);
     this.sceneSetup();
@@ -138,16 +169,86 @@ class BrainScene extends Component {
     const height = this.el.clientHeight;
 
     this.scene = new THREE.Scene();
+    // fov camera
     this.camera = new THREE.PerspectiveCamera(
         75, // fov = field of view
-        width / height, // aspect ratio
+        width / (height*2/3), // aspect ratio
         0.1, // near plane
         1000, // far plane
     );
-    this.camera.position.z = 200; // is used here to set some distance from a cube that is located at z = 0
+    this.camera.position.z = 140; // is used here to set some distance from a cube that is located at z = 0
     // OrbitControls allow a camera to orbit around the object
     // https://threejs.org/docs/#examples/controls/OrbitControls
     this.controls = new OrbitControls(this.camera, this.el);
+
+    const orthoWidth = width/3;
+    const orthoHeight = height/3;
+    const orthoAspect = orthoWidth/orthoHeight;
+    const createOrthographicCamera = (cameraDirection) => {
+      const orthoDistance = 120;
+      const orthoWidth = 60;
+      const left = -100*orthoAspect;
+      const right = 100*orthoAspect;
+      const top = 100;
+      const bottom = -100;
+      const near = orthoDistance - orthoWidth/2;
+      const far = orthoDistance + orthoWidth/2;
+      const camera = new THREE.OrthographicCamera(left, right, top, bottom, near, far);
+      switch (cameraDirection) {
+        case orthographicCameraDirection.X:
+          camera.position.x = 120;
+          camera.lookAt(new THREE.Vector3(-1, 0, 0));
+          break;
+        case orthographicCameraDirection.Y:
+          camera.position.y = 120;
+          camera.lookAt(new THREE.Vector3(0, -1, 0));
+          break;
+        case orthographicCameraDirection.Z:
+          camera.position.z = 120;
+          camera.lookAt(new THREE.Vector3(0, 0, -1));
+          break;
+      }
+      return camera;
+    };
+
+    const createPlanes = (cameraDirection) => {
+      const rotationByDirection = {
+        [orthographicCameraDirection.X]: [0, Math.PI/2, 0],
+        [orthographicCameraDirection.Y]: [Math.PI/2, 0, 0],
+        [orthographicCameraDirection.Z]: [0, 0, 0],
+      };
+
+      const geometry = new THREE.PlaneGeometry( 180, 180 );
+      const material = new THREE.MeshBasicMaterial( {color: 0xffff00, side: THREE.DoubleSide} );
+      material.opacity = 0.5;
+      material.transparent = true;
+      const planeNear = new THREE.Mesh( geometry, material );
+      const planeFar = new THREE.Mesh( geometry, material );
+      this.scene.add(planeNear);
+      planeNear.position.set(...planePositionByDirection[cameraDirection]([0, 0, 0], "near", initialCameraSpread/2));
+      planeNear.rotation.set(...rotationByDirection[cameraDirection]);
+      this.scene.add(planeFar);
+      planeFar.position.set(...planePositionByDirection[cameraDirection]([0, 0, 0], "far", initialCameraSpread/2));
+      planeFar.rotation.set(...rotationByDirection[cameraDirection]);
+
+      planeNear.visible = false;
+      planeFar.visible = false;
+      return [planeNear, planeFar];
+    };
+
+    // top camera
+    this.cameraTop = createOrthographicCamera(orthographicCameraDirection.Y);
+    const [topPlaneNear, topPlaneFar] = createPlanes(orthographicCameraDirection.Y);
+    // console.log(cameraTop);
+    // side camera
+    this.cameraSide = createOrthographicCamera(orthographicCameraDirection.X);
+    const [sidePlaneNear, sidePlaneFar] = createPlanes(orthographicCameraDirection.X);
+    // front camera
+    this.cameraFront = createOrthographicCamera(orthographicCameraDirection.Z);
+    const [frontPlaneNear, frontPlaneFar] = createPlanes(orthographicCameraDirection.Z);
+
+    this.setState({topPlaneFar, topPlaneNear, sidePlaneFar, sidePlaneNear, frontPlaneFar, frontPlaneNear});
+
     this.renderer = new THREE.WebGLRenderer({alpha: true});
     this.renderer.setClearColor( 0xffffff, 0);
     this.renderer.setSize(width, height);
@@ -244,7 +345,52 @@ class BrainScene extends Component {
   startAnimationLoop = () => {
     const delta = this.state.clock && this.state.clock.getDelta();
     // const elapsed = this.state.clock && this.state.clock.getElapsedTime();
+    const [left, bottom, topoWidth, topoHeight] = [0, 0,
+      Math.floor(this.el.clientWidth / 3),
+      Math.floor(this.el.clientHeight / 3)];
+    this.renderer.setViewport(left, bottom + topoHeight, this.el.clientWidth, this.el.clientHeight- topoHeight);
+    this.renderer.setScissor(left, bottom + topoHeight, this.el.clientWidth, this.el.clientHeight - topoHeight);
     this.renderer.render(this.scene, this.camera);
+
+    // hide all currently visible front and back planes
+    const visiblePlanes = [
+      this.state.topPlaneNear,
+      this.state.topPlaneFar,
+      this.state.frontPlaneNear,
+      this.state.frontPlaneFar,
+      this.state.sidePlaneNear,
+      this.state.frontPlaneFar,
+    ]
+        .filter((p) => p.visible === true);
+
+    visiblePlanes.forEach((p) => p.visible = false);
+
+    // top
+    this.renderer.setViewport(left, bottom, topoWidth, topoHeight);
+    this.renderer.setScissor(left, bottom, topoWidth, topoHeight);
+    this.renderer.setScissorTest(true);
+    this.renderer.setClearColor(new THREE.Color(1, 1, 1));
+    this.cameraTop.updateProjectionMatrix();
+    this.renderer.render(this.scene, this.cameraTop);
+
+    // front
+    this.renderer.setViewport(left + topoWidth, bottom, topoWidth, topoHeight);
+    this.renderer.setScissor(left + topoWidth, bottom, topoWidth, topoHeight);
+    this.renderer.setScissorTest(true);
+    this.renderer.setClearColor(new THREE.Color(1, 1, 1));
+    this.cameraFront.updateProjectionMatrix();
+    this.renderer.render(this.scene, this.cameraFront);
+
+    // side
+    this.renderer.setViewport(left + 2*topoWidth, bottom, topoWidth, topoHeight);
+    this.renderer.setScissor(left + 2*topoWidth, bottom, topoWidth, topoHeight);
+    this.renderer.setScissorTest(true);
+    this.renderer.setClearColor(new THREE.Color(1, 1, 1));
+    this.cameraSide.updateProjectionMatrix();
+    this.renderer.render(this.scene, this.cameraSide);
+
+    visiblePlanes.forEach((p) => p.visible = true);
+
     // The window.requestAnimationFrame() method tells the browser that you wish to perform
     // an animation and requests that the browser call a specified function
     // to update an animation before the next repaint
@@ -478,6 +624,80 @@ class BrainScene extends Component {
     this.initPoints();
   }
 
+  updateTopCamera = (centre) => {
+    clearTimeout(this.state.topPlaneTimeout);
+    this.cameraTop.near = -centre + this.state.orthoCameraDistance - (this.state.topCameraSpread/2);
+    this.cameraTop.far = -centre + this.state.orthoCameraDistance + (this.state.topCameraSpread/2);
+    this.cameraTop.updateProjectionMatrix();
+    this.setState({topCameraCentre: centre});
+    const [topPlaneNear, topPlaneFar] = [this.state.topPlaneNear, this.state.topPlaneFar];
+    topPlaneNear.position.set(...planePositionByDirection[orthographicCameraDirection.Y]([0,
+      centre,
+      0],
+    "near",
+    this.state.topCameraSpread/2));
+    topPlaneFar.position.set(...planePositionByDirection[orthographicCameraDirection.Y]([0,
+      centre,
+      0],
+    "far",
+    this.state.topCameraSpread/2));
+    topPlaneNear.visible = true;
+    topPlaneFar.visible = true;
+    this.setState({topPlaneTimeout: setTimeout(() => {
+      topPlaneNear.visible = false;
+      topPlaneFar.visible = false;
+    }, planeTimeoutMs)});
+  }
+  updateFrontCamera = (centre) => {
+    clearTimeout(this.state.frontPlaneTimeout);
+    this.cameraFront.near = -centre + this.state.orthoCameraDistance - (this.state.frontCameraSpread/2);
+    this.cameraFront.far = -centre + this.state.orthoCameraDistance + (this.state.frontCameraSpread/2);
+    this.cameraFront.updateProjectionMatrix();
+    this.setState({frontCameraCentre: centre});
+    const [frontPlaneNear, frontPlaneFar] = [this.state.frontPlaneNear, this.state.frontPlaneFar];
+    frontPlaneNear.position.set(...planePositionByDirection[orthographicCameraDirection.Z]([0,
+      0,
+      centre],
+    "near",
+    this.state.frontCameraSpread/2));
+    frontPlaneFar.position.set(...planePositionByDirection[orthographicCameraDirection.Z]([0,
+      0,
+      centre],
+    "far",
+    this.state.frontCameraSpread/2));
+    frontPlaneNear.visible = true;
+    frontPlaneFar.visible = true;
+    this.setState({frontPlaneTimeout: setTimeout(() => {
+      frontPlaneNear.visible = false;
+      frontPlaneFar.visible = false;
+    }, planeTimeoutMs)});
+  };
+
+  updateSideCamera = (centre) => {
+    clearTimeout(this.state.sidePlaneTimeout);
+    this.cameraSide.near = -centre + this.state.orthoCameraDistance - (this.state.sideCameraSpread/2);
+    this.cameraSide.far = -centre + this.state.orthoCameraDistance + (this.state.sideCameraSpread/2);
+    this.cameraSide.updateProjectionMatrix();
+    this.setState({sideCameraCentre: centre});
+    const [sidePlaneNear, sidePlaneFar] = [this.state.sidePlaneNear, this.state.sidePlaneFar];
+    sidePlaneNear.position.set(...planePositionByDirection[orthographicCameraDirection.X]([centre,
+      0,
+      0],
+    "near",
+    this.state.sideCameraSpread/2));
+    sidePlaneFar.position.set(...planePositionByDirection[orthographicCameraDirection.X]([centre,
+      0,
+      0],
+    "far",
+    this.state.sideCameraSpread/2));
+    sidePlaneNear.visible = true;
+    sidePlaneFar.visible = true;
+    this.setState({sidePlaneTimeout: setTimeout(() => {
+      sidePlaneNear.visible = false;
+      sidePlaneFar.visible = false;
+    }, planeTimeoutMs)});
+  }
+
   render() {
     return <Ref innerRef={this.contextRef}>
       <Grid centered columns={3}>
@@ -524,6 +744,55 @@ class BrainScene extends Component {
             />
           </PageSidebar>
         </GridColumn>
+        <GridColumn width={4}>
+          <Segment vertical>
+            <Header>Top camera</Header>
+            <Slider
+              value={this.state.topCameraCentre}
+              color="red"
+              settings={{
+                start: 0,
+                min: -120.0,
+                max: 120.0,
+                step: 1,
+                onChange: this.updateTopCamera,
+              }}
+            />
+          </Segment>
+        </GridColumn>
+        <GridColumn width={4}>
+          <Segment vertical>
+            <Header>Front camera</Header>
+            <Slider
+              value={this.state.frontCameraCentre}
+              color="red"
+              settings={{
+                start: 0,
+                min: -120.0,
+                max: 120.0,
+                step: 1,
+                onChange: this.updateFrontCamera,
+              }}
+            />
+          </Segment>
+        </GridColumn>
+        <GridColumn width={4}>
+          <Segment vertical>
+            <Header>Side camera</Header>
+            <Slider
+              value={this.state.sideCameraCentre}
+              color="red"
+              settings={{
+                start: 0,
+                min: -120.0,
+                max: 120.0,
+                step: 1,
+                onChange: this.updateSideCamera,
+              }}
+            />
+          </Segment>
+        </GridColumn>
+        <GridColumn width={4}/>
       </Grid>
     </Ref>;
   }
